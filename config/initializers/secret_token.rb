c31c3732645b9b5bfdde7b37148fbb2de71a115e
Tucker Roth <troth@tem-troth-dnv.local>
2012-01-24 11:34:36 -0700

    Initial add

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CookieScout::Application.config.secret_token = '078e8c6f36bce7a8cc2f9cb5efc396286b0fb4233653b4aefde68ef0e1562c25b5f8ea293c7e0b47b27e2d048b776d11dcdfc926057b3bf9068dcbf6e1987b22'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CookieScout::Application.config.session_store :cookie_store, key: '_CookieScout_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CookieScout::Application.config.session_store :active_record_store
