57c70064c62fd82ab16d1893a358a4a028648e69
Tucker Roth <troth@thoughtequity.com>
2012-01-26 09:49:46 -0700

    Added inventory totals and spiced up UI (a bit)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,5 +9,19 @@ class HomeController < ApplicationController
     #  Get summary data about Inventory
     @product_delivery_summary = ProductDelivery.summary
     @product_delivery_total = ProductDelivery.total
+
+    # Find how much product we have left-over
+    @product_left = Hash.new
+    @product_delivery_summary.each_pair do |pKey,pNumDelivered|
+       @order_summary.each_pair do |orderKey,orderNumOrdered|
+         #  Find the proper entry in orders that matches deliveries
+         if pKey == orderKey
+           # subtract number of products ordered from the number delivered
+           product_left_over = Integer(pNumDelivered) - Integer(orderNumOrdered)
+           @product_left[pKey] = product_left_over.to_s
+         end
+       end
+    end
+
   end
 end
